538a1c4a14866981f133b70916657c64a1b47096
soulincsl <soulincsl@gmail.com>
2023-02-21 18:01:15 +0700

    new commit

--- pages/detail.vue
+++ pages/detail.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container m-auto">
         <div class="grid">
-            <h1>Hello world</h1>
+            <img src="https://dummyimage.com/600x400/000/fff" />
         </div>
         <section class="mb-16 relative">
             <div class="relative z-10">
@@ -10,21 +10,16 @@
                         <ul
                             class="border-b border-slate-200 space-x-6 flex whitespace-nowrap dark:border-slate-200/5"
                         >
-                            <li>
+                            <li v-for="(item, index) in tabs" :key="index">
                                 <h2>
                                     <a
-                                        class="flex text-sm leading-6 font-semibold pt-3 pb-2.5 border-b-2 -mb-px text-blue-500 border-current"
-                                        href="/docs/installation"
-                                        >Overview</a
-                                    >
-                                </h2>
-                            </li>
-                            <li>
-                                <h2>
-                                    <a
-                                        class="flex text-sm leading-6 font-semibold pt-3 pb-2.5 border-b-2 -mb-px text-slate-900 border-transparent hover:border-slate-300 dark:text-slate-200 dark:hover:border-slate-700"
-                                        href="/docs/installation/"
-                                        >Room</a
+                                        :class="`flex text-sm leading-6 font-semibold pt-3 pb-2.5 border-b-2 -mb-px ${
+                                            item.value === tab
+                                                ? 'text-blue-500 border-current'
+                                                : 'text-slate-900 border-transparent hover:border-slate-300'
+                                        }`"
+                                        :href="`/detail?tab=${item.value}`"
+                                        >{{ item.name }}</a
                                     >
                                 </h2>
                             </li>
@@ -37,14 +32,29 @@
                 class="relative z-10 max-w-3xl mb-16 prose prose-slate dark:prose-dark"
                 bis_skin_checked="1"
             >
-                <p>
-                    The simplest and fastest way to get up and running with
-                    Tailwind CSS from scratch is with the Tailwind CLI tool. The
-                    CLI is also available as a
-                    <a href="/blog/standalone-cli">standalone executable</a> if
-                    you want to use it without installing Node.js.
-                </p>
+                <div v-if="tab === '1'">
+                    <h1>Overview content</h1>
+                </div>
+                <div v-if="tab === '2'">
+                    <h1>Location content</h1>
+                </div>
+                <div v-if="tab === '3'">
+                    <h1>Policy</h1>
+                </div>
             </div>
         </section>
     </div>
 </template>
+
+<script setup>
+const route = useRoute();
+const tab = ref(route.query.tab ? route.query.tab : "1");
+const tabs = ref([
+    {
+        name: "Overview",
+        value: "1",
+    },
+    { name: "Location", value: "2" },
+    { name: "Policy", value: "3" },
+]);
+</script>
